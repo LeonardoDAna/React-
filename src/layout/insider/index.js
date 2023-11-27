ff6810c5a65bc190ff84419375570df2af387e9b
LeonardeDana <leonardeDana@gmail.com>
2023-11-27 17:25:25 +0800

    学习 hooks

--- src/layout/insider/index.js
+++ src/layout/insider/index.js
@@ -1,11 +1,35 @@
-
+import React from "react";
 const Insider = () => {
+  const [count, setCount] = React.useState([0]);
+  const [options, setOptions] = React.useState([]);
+  const [option, setOption] = React.useState("");
+  const inputRef = React.createRef();
+  const add = () => {
+    let res = parseInt(count) + 1;
+    let newOptions = options;
+    newOptions.push(option);
+    setCount(res);
+    console.log(options, newOptions);
+    setOptions(newOptions);
+  };
+
+  React.useEffect(() => {}, [options]);
+
+  const inputOptionChange = (e) => {
+    setOption(e.target.value);
+    console.log(e.target.value);
+  };
   return (
     <div className="containerRoot">
       <p>Insider</p>
 
-      <input></input>
-      <button onClick={}>Add</button>
+      <input ref={inputRef} value={option} onChange={inputOptionChange}></input>
+      <button onClick={add}>Add</button>
+      <ul>
+        {options.map((e) => {
+          <li>{e}</li>;
+        })}
+      </ul>
     </div>
   );
 };
